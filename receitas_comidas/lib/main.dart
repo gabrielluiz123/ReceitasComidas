4a69f92acd4120d6f7df3735e76779074ac73380
Gabriel Luiz <gabriells760@gmail.com>
2020-09-07 20:27:17 -0300

    First Commit
    Commit inicial do sistema de refeições, com menu inicial e roteamento

---
+++ receitas_comidas/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'screens/categorys_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        accentColor: Colors.amber,
+        fontFamily: 'Releway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+      routes: {
+        AppRoutes.CATEGORIESMEALS: (ctx) => CategoriesMealsScreen(),
+      },
+    );
+  }
+}
